abdb9b6169d9e4f57f9f504fa34878ce8b6add6c
Viktoria Sernevych <v.sernevich@gmail.com>
2016-07-14 17:03:44 +0300

    Add commentary to code

--- TaskOne/TaskMathPow.js
+++ TaskOne/TaskMathPow.js
@@ -3,19 +3,18 @@
  */
 
 
-var strNumb = (prompt("Enter the number", ""));
+var strNumb = (prompt("Enter the number", ""));// user's input in String format
 var strDegree = (prompt("Enter the degree", ""));
-
+// check for valid input if it's empty string pf not a number
 while (strNumb == "" || isNaN(strNumb) || strDegree == "" || isNaN(strDegree)) {
-    debugger;
+    // if it's ask user to enter value again do it until the valid value will be entered
     strNumb = (prompt("Please, enter the number", ""));
     strDegree = (prompt("Please, enter the degree", ""));
 }
-var numNumb = +strNumb;
+var numNumb = +strNumb;          // convert string to number,
 var numDegree = +strDegree;
-console.log("Type of num is ", typeof numDegree);
-if (numDegree == 0) {
-    console.log("Result is 1");
+if (numDegree == 0) {            // if degree = 0, return 1 because every number in degree 0 equals 1
+    console.log("Result is 1");  // we know it without calculations
 }
 else {
     var result = 1;
