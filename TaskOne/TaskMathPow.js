0e3f50118fd26694bdce40be4aaf1a4057a08da3
Viktoria Sernevych <v.sernevich@gmail.com>
2016-07-14 16:05:13 +0300

    first task math pow added

---
+++ TaskOne/TaskMathPow.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Viktoria on 13.07.2016.
+ */
+
+
+var strNumb = (prompt ("Enter the number",""));
+var strDegree = (prompt ("Enter the degree",""));
+
+while (strNumb == ""|| isNaN(strNumb) || strDegree == ""|| isNaN(strDegree)){
+    debugger;
+    strNumb = (prompt ("Please, enter the number",""));
+     strDegree = (prompt ("Please, enter the degree",""));
+}
+var numNumb = +strNumb;
+var numDegree =+strDegree;
+console.log("Type of num is ", typeof numDegree );
+ if (numDegree == 0){
+    console.log("Result is 1");
+}
+else{
+    var result = 1;
+    while(numDegree > 0){
+          result *= numNumb;
+        numDegree--;
+    }
+}
+
+console.log("Result is", result);
